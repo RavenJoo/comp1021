ef86eb5d085424b4a238d191dae4bb98d8f2f27a
RavenJoo <melody10511@gmail.com>
2014-04-15 22:16:04 +0900

    Completed lab 7 and 8
    Actual starting point for the project 2

--- Shooting Game/Shooting Game.py
+++ Shooting Game/Shooting Game.py
@@ -28,7 +28,7 @@ enemy_size = 50         # The size of the enemy image plus margin
 enemy_init_x = -window_width / 2 + window_margin
 enemy_init_y = window_height / 2 - window_margin
 enemy_min_x = enemy_init_x
-enemy_max_x = window_width / 2 - enemy_size * enemy_number
+enemy_max_x = window_width / 2 - enemy_size * 6
     # The maximum x coordinate of the first enemy, which will be used
     # to restrict the x coordinates of all other enemies
 enemy_hit_player_distance = 30
@@ -69,12 +69,10 @@ def playermoveleft():
     # Get current player position
     x, y = player.position()
 
-    # Part 2.2 - Keeping the player inside the window
     # Player should only be moved only if it is within the window range
+    if x - player_speed > -window_width / 2 + window_margin:
+        player.goto(x - player_speed, y)
 
-    player.goto(x - player_speed, y)
-
-    turtle.update() # delete this line after finishing updatescreen()
 
 # This function is run when the "Right" key is pressed. The function moves the
 # player to the right when the player is within the window area
@@ -83,12 +81,10 @@ def playermoveright():
     # Get current player position
     x, y = player.position()
 
-    # Part 2.2 - Keeping the player inside the window
     # Player should only be moved only if it is within the window range
+    if x + player_speed < window_width / 2 - window_margin:
+        player.goto(x + player_speed, y)
 
-    player.goto(x + player_speed, y)
-
-    turtle.update() # delete this line after finishing updatescreen()
 
 """
     Handle the screen update and enemy movement
@@ -108,66 +104,81 @@ def updatescreen():
     # position of enemy at the top left corner, enemy_min_x and enemy_max_x
 
     # x and y displacements for all enemies
-    dx = enemy_speed
+    dx = enemy_speed * enemy_direction
     dy = 0
 
-    # Part 3.3
     # Perform several actions if the enemies hit the window border
-
+    x0 = enemies[0].xcor()
+    if x0 + dx > enemy_max_x or x0 + dx < enemy_min_x:
         # Switch the moving direction
+        enemy_direction = -enemy_direction
 
         # Bring the enemies closer to the player
+        dy = -enemy_size / 2
 
         # Increase the speed when the direction switches to right again
+        if enemy_direction == 1:
+            enemy_speed = enemy_speed + enemy_speed_increment
 
     # Move the enemies according to the dx and dy values determined above
     for enemy in enemies:
         x, y = enemy.position()
         enemy.goto(x + dx, y + dy)
 
-    # Part 4.3 - Moving the laser
+        # Set up alternating enemy image
+        if (x//20)%2 == 0:
+            enemy.shape("enemy.gif")
+        else:
+            enemy.shape("enemy2.gif")
+        
     # Perfrom several actions if the laser is visible
 
     if laser.isvisible():
         # Move the laser
+        x, y = laser.position()
+        laser.goto(x, y + laser_speed)
 
         # Hide the laser if it goes beyong the window
+        if laser.ycor() > window_height / 2:
+            laser.hideturtle()
 
         # Check the laser against every enemy using a for loop
         for enemy in enemies:
             # If the laser hits a visible enemy, hide both of them
-
+            if enemy.isvisible() and laser.distance(enemy) < laser_hit_enemy_distance:
+                enemy.hideturtle()
+                laser.hideturtle()
                 # Stop if some enemy is hit
                 break
 
-    # Part 5.1 - Gameover when one of the enemies is close to the player
-
     # If one of the enemies is very close to the player, the game will be over
     for enemy in enemies:
-        if enemy.ycor()-player.ycor() < enemy_hit_player_distance:
+        if enemy.ycor()-player.ycor() < enemy_hit_player_distance and enemy.isvisible():
             # Show a message
             gameover("You lose!")
 
             # Return and do not run updatescreen() again
             return
 
-    # Part 5.2 - Gameover when you have killed all enemies
-
     # Set up a variable as a counter
+    count = 0
 
     # For each enemy
-
+    for enemy in enemies:
         # Increase the counter if the enemy is visible
+        if enemy.isvisible():
+            count = count + 1
 
     # If the counter is 0, that means you have killed all enemies
-
+    if count == 0:
         # Perform several gameover actions
+        gameover("You win!")
 
-    # Part 3.1 - Controlling animation using the timer event
+    # Update the screen
+    turtle.update()
 
-    #
-    # Add code here
-    #
+    # Schedule the next screen update
+    turtle.ontimer(updatescreen, update_interval)
 
 """
     Shoot the laser
@@ -178,20 +189,26 @@ def updatescreen():
 # be shot at any one time.
 def shoot():
 
-    print("Pyoo!") # delete this line after completing the function
-
-    # Part 4.2 - the shooting function
     # Shoot the laser only if it is not visible
-
-    #
-    # Add code here
-    #
+    if not laser.isvisible():
+        # Make the laser to become visible
+        laser.showturtle()
+        
+        # Move the laser to the position of the player
+        laser.goto(player.pos())
 
 """
     Game start
 """
 # This function contains things that have to be done when the game starts.
-def gamestart():
+def gamestart(x, y):
+    start_button.clear()
+    start_button.hideturtle()
+
+    labels.clear()
+    enemy_number_text.clear()
+    left_arrow.hideturtle()
+    right_arrow.hideturtle()
     # Use the global variables here because we will change them inside this
     # function
     global player, laser
@@ -207,17 +224,17 @@ def gamestart():
     player.up()
     player.goto(player_init_x, player_init_y)
 
-    # Part 2.1
     # Map player movement handlers to key press events
 
-    #
-    # Add code here
-    #
+    turtle.onkeypress(playermoveleft, "Left")
+    turtle.onkeypress(playermoveright, "Right")
+    turtle.listen()
 
     ### Enemy turtles ###
 
     # Add the enemy picture
     turtle.addshape("enemy.gif")
+    turtle.addshape("enemy2.gif")
 
     for i in range(enemy_number):
         # Create the turtle for the enemy
@@ -226,7 +243,7 @@ def gamestart():
         enemy.up()
 
         # Move to a proper position counting from the top left corner
-        enemy.goto(enemy_init_x + enemy_size * i, enemy_init_y)
+        enemy.goto(enemy_init_x + enemy_size * (i % 6), enemy_init_y - enemy_size * (i // 6))
 
         # Add the enemy to the end of the enemies list
         enemies.append(enemy)
@@ -246,19 +263,12 @@ def gamestart():
     # Hide the laser turtle
     laser.hideturtle()
 
-    # Part 4.2 - Mapping the shooting function to key press event
-
-    #
-    # Add code here
-    #
+    turtle.onkeypress(shoot, "space")
 
     turtle.update()
 
-    # Part 3.1 - Controlling animation using the timer event
-
-    #
-    # Add code here
-    #
+    # Start the game by running updatescreen()
+    turtle.ontimer(updatescreen, update_interval)
 
 
 """
@@ -269,8 +279,11 @@ def gamestart():
 def gameover(message):
 
     # Part 5.3 - Improving the gameover() function
-
-    print(message) # delete this line after completing the function
+    goturtle = turtle.Turtle()
+    goturtle.hideturtle()
+    goturtle.pencolor("yellow")
+    goturtle.write(message, align="center", font=("System", 30, "bold"))
+    turtle.update()
 
 """
     Set up main Turtle parameters
@@ -283,8 +296,78 @@ turtle.up()
 turtle.hideturtle()
 turtle.tracer(False)
 
-# Start the game
-gamestart()
+# Spinner control initialization
+labels = turtle.Turtle()
+labels.hideturtle()
+labels.pencolor("White")
+labels.up()
+# Write the text
+labels.goto(-100, 0) # Next to the spinner control
+labels.write("Number of Enemies:", font=("System", 12, "bold"))
+# Value display
+enemy_number_text = turtle.Turtle()
+enemy_number_text.hideturtle()
+enemy_number_text.pencolor("White")
+enemy_number_text.up()
+enemy_number_text.goto(80, 0)
+enemy_number_text.write(str(enemy_number), font=("System", 12, "bold"), align="center")
+# Left arrow
+left_arrow = turtle.Turtle()
+left_arrow.up()
+left_arrow.shape("arrow")
+left_arrow.color("White")
+left_arrow.shapesize(0.5, 1)
+left_arrow.left(180)
+left_arrow.goto(60, 8)
+def decrease_enemy_number(x, y):
+    # Declare enemy_number as global
+    global enemy_number
+    if enemy_number > 1:
+        enemy_number = enemy_number - 1
+        enemy_number_text.clear()
+        enemy_number_text.write(str(enemy_number), font=("System", 12, "bold"), align="center")
+left_arrow.onclick(decrease_enemy_number)
+left_arrow.showturtle()
+# Right arrow
+right_arrow = turtle.Turtle()
+right_arrow.up()
+right_arrow.shape("arrow")
+right_arrow.color("White")
+right_arrow.shapesize(0.5, 1)
+right_arrow.goto(100, 8)
+def increase_enemy_number(x, y):
+    # Declare enemy_number as global
+    global enemy_number
+    if enemy_number < 48:
+        enemy_number = enemy_number + 1
+        enemy_number_text.clear()
+        enemy_number_text.write(str(enemy_number), font=("System", 12, "bold"), align="center")
+right_arrow.onclick(increase_enemy_number)
+
+# Start button initialization
+start_button = turtle.Turtle()
+start_button.up()
+start_button.goto(-40, -40)
+# Set up button graphics
+start_button.color("White", "DarkGray")
+start_button.begin_fill()
+for _ in range(2):
+    start_button.forward(80)
+    start_button.left(90)
+    start_button.forward(25)
+    start_button.left(90)
+start_button.end_fill()
+start_button.color("White")
+start_button.goto(0, -35)
+start_button.write("Start", font=("System", 12, "bold"), align = "center")
+# Make the turtle clickable with the same size as the graphics
+start_button.goto(0, -28)
+start_button.shape("square")
+start_button.shapesize(1.25, 4)
+start_button.color("")
+start_button.onclick(gamestart)
+
+turtle.update()
 
 # Switch focus to turtle graphics window
 turtle.done()
